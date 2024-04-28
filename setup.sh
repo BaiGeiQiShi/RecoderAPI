507bb9ad0780b4ec3deb29569b7d6070a06ed594
WhiteKngiht <67951843+BaiGeiQiShi@users.noreply.github.com>
2024-04-29 02:38:02 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ pip3 install -r requirements.txt
 cp .bashrc ~/.bashrc
 source ~/.bashrc
 
+echo "export PATH=/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> ~/.bashrc
+
 cp -r ~/Repair/checkpointSearch ./
 
 cd /
@@ -14,3 +16,4 @@ echo "export PATH=\$PATH:/defects4j" >> ~/.bashrc && export PATH=$PATH:/defects4
 git clone https://github.com/universetraveller/CatenaD4J.git
 echo "export PATH=\$PATH:/CatenaD4J" >> ~/.bashrc && export PATH=$PATH:/CatenaD4J
 
+source ~/.bashrc
