5bb91cbf5b9a567ccf990da26f80a59cbb01abb9
BaiGeiQiShi <1014813743@qq.com>
2024-04-29 04:54:06 +0800

    change the environment setup method

--- setup.sh
+++ setup.sh
@@ -1,19 +1,30 @@
-#!/bin/bash -x
-
+#!/bin/bash -i
+echo "======================================="
+echo "@: Please input y yes ENTER if the installations request"
+echo "================Set up================="
+echo "@: Detecting conda installation"
+if test -z "$(conda -V)"; then
+	echo "@: Trying to install conda with python3.9"
+	echo "@: Updating apt-get"
+	echo "=================="
+	apt-get update
+	echo "@: Trying to install wget"
+	echo "=================="
+	apt-get install wget
+	echo "@: Download conda with wget now"
+	wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh -O ~/anaconda.sh
+	bash ~/anaconda.sh -p $HOME/anaconda
+	rm ~/anaconda.sh
+	source ~/.bashrc
+fi
+echo "@: You have installed $(conda -V)"
+echo "@: Please ensure your python version is advanced than 3.7 to use the tool"
+echo "@: Trying to install cudatoolkit, ignore it if you have installed"
+conda install cudatoolkit==11.3.1
+echo "@: Trying to install cudnn"
+conda install cudnn==8.2.1
+echo "@: Trying to install torch"
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+echo "@: Installing requirements"
 pip3 install -r requirements.txt
-cp .bashrc ~/.bashrc
-source ~/.bashrc
-
-echo "export PATH=/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> ~/.bashrc
-
-cp -r ~/Repair/checkpointSearch ./
-
-cd /
-
-git clone https://github.com/rjust/defects4j.git
-echo "export PATH=\$PATH:/defects4j" >> ~/.bashrc && export PATH=$PATH:/defects4j
-
-git clone https://github.com/universetraveller/CatenaD4J.git
-echo "export PATH=\$PATH:/CatenaD4J" >> ~/.bashrc && export PATH=$PATH:/CatenaD4J
-
-source ~/.bashrc
+echo "END OF SCRIPT"
