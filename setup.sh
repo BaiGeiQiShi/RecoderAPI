5ce38e77dfd5b6c29a25ca3f696e840d586c5278
WhiteKngiht <67951843+BaiGeiQiShi@users.noreply.github.com>
2024-04-29 02:04:52 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
+
 pip3 install -r requirements.txt
+cp .bashrc ~/.bashrc
+source ~/.bashrc
+
 cp -r ~/Repair/checkpointSearch ./
+
+git clone https://github.com/rjust/defects4j.git
+echo "export PATH=\$PATH:/defects4j" >> ~/.bashrc && export PATH=$PATH:/defects4j
+
+git clone https://github.com/universetraveller/CatenaD4J.git
+echo "export PATH=\$PATH:/CatenaD4J" >> ~/.bashrc && export PATH=$PATH:/CatenaD4J
+
